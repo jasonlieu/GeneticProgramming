e3d9f9afc0b708efde661f4d4916e243e24ed01a
jasonlieu <jasonlieuthefirst@gmail.com>
2018-11-08 05:53:59 -0800

    add mutate leaf, no mutate op yet

--- GP.lisp
+++ GP.lisp
@@ -46,7 +46,52 @@
     )
     (RETURN-FROM PURGE survivors)
 )
-
+(DEFUN MUTATE(expression)
+    (IF (LISTP expression)                              ;only mutate lists, not leaves
+        (PROGN
+            (SETF *random-state* (make-random-state t))                  
+            (IF (= 0 (RANDOM 20))                            ;0 = mutation, 1 - 19 = no mutation
+                (IF (= 0 (RANDOM 1))                  ;0 = leaf, 1 = op
+                    (PROGN                              ;leaf mutation
+                        (IF (AND (LISTP (NTH 1 expression)) (LISTP (NTH 2 expression)))     ;if no leaves, keep recursing
+                            (RETURN-FROM MUTATE (LIST (NTH 0 expression) (MUTATE (NTH 1 expression)) (MUTATE (NTH 2 expression))))
+                            (PROGN
+                                (SETF leafXYZConst (RANDOM 4))  ;choose leaf replacement: X Y Z [-9 to 9]
+                                (IF (= leafXYZConst 0)
+                                    (SETF replacement 'X)
+                                (IF (= leafXYZConst 1)
+                                    (SETF replacement 'Y)
+                                (IF (= leafXYZConst 2)
+                                    (SETF replacement 'Z)
+                                (IF (= (RANDOM 2) 0)
+                                        (SETF replacement (RANDOM 9))
+                                        (SETF replacement (- 0 (RANDOM 9)))
+                                ))))
+                                (IF (= (RANDOM 2) 0)            ;choose which leaf to replace
+                                    (IF (LISTP (NTH 1 expression))  ;only replace if NTH 1 is a leaf
+                                        (RETURN-FROM MUTATE (LIST (NTH 0 expression) (MUTATE (NTH 1 expression)) (MUTATE (NTH 2 expression))))
+                                        (RETURN-FROM MUTATE (LIST (NTH 0 expression) replacement (MUTATE (NTH 2 expression))))
+                                    )
+                                    (IF (LISTP (NTH 2 expression))  ;only replace if NTH 2 is a leaf
+                                        (RETURN-FROM MUTATE (LIST (NTH 0 expression) (MUTATE (NTH 1 expression)) (MUTATE (NTH 2 expression))))
+                                        (RETURN-FROM MUTATE (LIST (NTH 0 expression) (MUTATE (NTH 1 expression)) replacement ))
+                                    )
+                                )
+                            )
+                        )
+                    )
+                    (PROGN
+                    ;   op replacement here
+                    ;   don't have to worry about whether or not it's a leaf
+                    ;   change line 54 to (RANDOM 2)
+                    )
+                )
+                (RETURN-FROM MUTATE (LIST (NTH 0 expression) (MUTATE (NTH 1 expression)) (MUTATE (NTH 2 expression)))) ;no mutation, keep recursing
+            )
+        )
+        (RETURN-FROM MUTATE expression) ;expression is already a leaf, stop mutation
+    )
+)
 ;MAIN
 (SETF Y 4)                  ;set XYZ and expected
 (SETF X 2)
@@ -57,10 +102,9 @@
     do
     (SETF pool (APPEND pool (LIST (GENERATE 0))))
 )
-(SETF nextGen (PURGE pool expected 24))          ;purge, keep 25
+(SETF pool (PURGE pool expected 24))          ;purge, keep 25
 
 
-(LOOP for count from 0 to 24            ;test, print survivors expressions and fitness
-    do
-    (PRINT (LIST (NTH count nextGen) "      " (ABS (- expected (EVAL (NTH count nextGen))))))
-)
+;testing
+(PRINT (NTH 0 pool))
+(PRINT (MUTATE (NTH 0 pool)))
